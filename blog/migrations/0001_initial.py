a98c4d21ce962c7b03501b4ed401b2326cf123bc
Dmytro Ivanytskyi <dima.ivanic@gmail.com>
2022-11-27 11:20:14 +0200

    fifth commit"

--- blog/migrations/0001_initial.py
+++ blog/migrations/0001_initial.py
@@ -3,7 +3,7 @@
 from django.conf import settings
 from django.db import migrations, models
 import django.db.models.deletion
-import django.utils.timezone
+from django.utils import timezone
 
 
 class Migration(migrations.Migration):
@@ -21,8 +21,8 @@ class Migration(migrations.Migration):
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('title', models.CharField(max_length=100)),
                 ('text', models.TextField()),
-                ('creation_date', models.DateField(default=django.utils.timezone.now)),
-                ('publishing_date', models.DateField(blank=True, null=True)),
+                ('created_date', models.DateTimeField(default=timezone.now)),
+                ('published_date', models.DateTimeField(blank=True, null=True)),
                 ('author', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
             ],
         ),

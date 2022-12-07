62418df830602f05a408632839f11b91f19c745f
Dmytro Ivanytskyi <dima.ivanic@gmail.com>
2022-12-07 18:11:01 +0200

    complete w asyncio

---
+++ client.py
@@ -0,0 +1,144 @@
+import asyncio
+import curses
+import string
+from itertools import chain
+
+
+class ChatWindow:
+
+    HOST = '0.0.0.0'
+    PORT = 1717
+    TITLE = 'Chat(d)'
+    USERNAME = 'Dmytro Ivanytskyi'
+
+    def __init__(self):
+        self.reader = None
+        self.writer = None
+        self.stdscr = curses.initscr() #type:ignore
+        self.cursor_x = 0
+        self.current_message = ''
+        self.messages = []
+
+    def start(self):
+        self.stdscr.clear()
+        self.stdscr.refresh()
+        curses.noecho() #type:ignore
+        curses.cbreak() #type:ignore
+        self.stdscr.keypad(True)
+        curses.start_color() #type:ignore
+        curses.init_pair(1, curses.COLOR_CYAN, curses.COLOR_BLACK) #type:ignore
+        curses.init_pair(2, curses.COLOR_BLUE, curses.COLOR_BLACK) #type:ignore
+        curses.init_pair(3, curses.COLOR_BLACK, curses.COLOR_WHITE) #type:ignore
+
+    def stop(self):
+        curses.nocbreak() #type:ignore
+        self.stdscr.keypad(False)
+        curses.echo() #type:ignore
+        self.writer.close() #type:ignore 
+
+    def get_message_text(self, message):
+        message = message.strip()
+        message = message.replace('\t', '    ')
+        return f'[{self.USERNAME}]: {message}'
+
+    def format_message(self, message):
+        lines = 0
+        current_line_width = 0
+        result = ''
+        for c in message:
+            current_line_width += 1
+            if c == '\n':
+                current_line_width = 0
+                lines += 1
+            elif current_line_width == self.width - 2:
+                result += '\n'
+                lines += 1
+                current_line_width = 1
+            result += c
+        return result, lines, current_line_width
+
+    @property
+    def height(self):
+        return self.stdscr.getmaxyx()[0]
+
+    @property
+    def width(self):
+        return self.stdscr.getmaxyx()[1]
+
+    def draw_title(self):
+        self.stdscr.attron(curses.color_pair(3)) #type:ignore
+        self.stdscr.attron(curses.A_BOLD) #type:ignore
+        title = f'Welcome, {self.USERNAME}'
+        start_x_title = int((self.width // 2) - (len(title) // 2) - len(title) % 2)
+        self.stdscr.addstr(0, start_x_title, title)
+        self.stdscr.attroff(curses.color_pair(3)) #type:ignore
+        self.stdscr.attroff(curses.A_BOLD) #type:ignore
+
+    def draw_messages(self):
+        result = []
+
+        current_message_text = self.get_message_text(self.current_message)
+        input_text, lines, last_line_size = self.format_message(current_message_text)
+
+        for message in self.messages:
+            formatted_message = self.format_message(message)[0]
+            result.extend(formatted_message.split('\n'))
+
+        lines_to_draw = self.height - lines - 4
+
+        messages = result[-lines_to_draw:]
+        self.stdscr.addstr(2, 0, '\n'.join(messages))
+
+    def draw_input(self):
+        self.stdscr.attron(curses.color_pair(2)) #type:ignore
+        message_text = self.get_message_text(self.current_message)
+        input_text, lines, last_line_size = self.format_message(message_text)
+        self.stdscr.addstr(self.height - lines - 1, 0, input_text)
+        self.stdscr.move(self.height - 1, last_line_size)
+        self.stdscr.attroff(curses.color_pair(2)) #type:ignore
+
+    def redraw(self):
+        self.stdscr.clear()
+        self.draw_title()
+        self.draw_messages()
+        self.draw_input()
+        self.stdscr.refresh()
+
+    def send_message(self):
+        self.messages.append(self.get_message_text(self.current_message))
+        self.writer.write(self.get_message_text(self.current_message).encode()) #type:ignore
+        self.current_message = ''
+        self.redraw()
+
+    async def run(self):
+        try:
+            c = 0
+            while True:
+                if c == ord('\n'):
+                    self.send_message()
+                    self.redraw()
+                elif c == curses.KEY_BACKSPACE: #type:ignore
+                    self.current_message = self.current_message[:-1]
+                elif c != 0:
+                    char = chr(c)
+                    if char in chain(string.digits, string.ascii_letters, string.whitespace):
+                        self.current_message += char
+                self.redraw()
+                c = self.stdscr.getch()
+        finally:
+            self.stop()
+
+    async def receive_message(self):
+        while True:
+            data = await self.reader.read(1024) #type:ignore
+            self.messages.append(data.decode().split('\n'))
+
+    async def main(self):
+        self.reader, self.writer = await asyncio.open_connection(self.HOST, self.PORT)
+        self.start()
+        await asyncio.gather(self.run(), self.receive_message())
+
+
+if __name__ == '__main__':
+    chat = ChatWindow()
+    asyncio.run(chat.main())
